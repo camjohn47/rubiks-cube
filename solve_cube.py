3b4e92f0cc0b8ed01c5a23b4b553156c52dec197
camjohn47 <camjohn@g.ucla.edu>
2021-02-15 15:05:08 -0800

    Removed unneccessary packages from solve_cube.py.

--- solve_cube.py
+++ solve_cube.py
@@ -1,9 +1,6 @@
 from rubikscube import RubiksCube
-import itertools 
-import random
-import sys
 
 distance, seed = 8, 30
 cube = RubiksCube(distance=distance, seed=seed)
-cube.solve(depth=4)
+cube.solve()
 
